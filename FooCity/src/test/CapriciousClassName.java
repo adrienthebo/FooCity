c461d36c0102bd217b0818d27c91209c7b3f8131
Adrien Thebo <adrien@puppetlabs.com>
2012-02-25 15:43:58 -0800

    Add test suite framework

---
+++ FooCity/src/test/CapriciousClassName.java
@@ -0,0 +1,21 @@
+package test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import test.tile.*;
+/**
+ * <p>
+ * This class is a placeholder for the the JUnit test suite annotations.
+ * </p>
+ *
+ * <p>
+ * More details at http://radio.javaranch.com/lasse/2006/07/27/1154024535662.html
+ * </p>
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        TestTileDelegation.class,
+        TestTileGetSubTile.class
+})
+public class CapriciousClassName {}
