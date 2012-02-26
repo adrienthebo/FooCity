4db5b624bdea5d14ab6989461ba320ae3c9e78bf
Adrien Thebo <adrien@puppetlabs.com>
2012-02-26 11:41:26 -0800

    More stubbing out in gamestate

--- FooCity/src/foocity/GameState.java
+++ FooCity/src/foocity/GameState.java
@@ -5,6 +5,29 @@ import foocity.grid.Grid;
 public class GameState {
 	protected Grid _grid;
 	protected TaxRates _taxes;
+	protected GameCalendar _calendar;
 	
+	protected int _funds;
+	/*
+	 * XXX I'm not sure if funds should be a separate class; it might make
+	 * sense to do so to isolate the logic of purchasing things and whatnot
+	 * into a different class. It would also make adding a listener for funds
+	 * simpler. On the other hand, it's just a ****ing integer.
+	 */
 	
+	public Grid getGrid() {
+		return _grid;
+	}
+	
+	public TaxRates getTaxRates() {
+		return _taxes;
+	}
+	
+	public GameCalendar getCalendar() {
+		return _calendar;
+	}
+	
+	public int getFunds() {
+		return _funds;
+	}
 }
