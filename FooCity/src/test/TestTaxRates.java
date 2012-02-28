f01bd9d3abc9eb860d6cba91143bed517472483a
Adrien Thebo <adrien@puppetlabs.com>
2012-02-28 09:08:22 -0800

    Add tests for TaxRates attributes

--- FooCity/src/test/TestTaxRates.java
+++ FooCity/src/test/TestTaxRates.java
@@ -28,4 +28,64 @@ public class TestTaxRates {
 		assertEquals(30, _subject.getBusinessTax());
 		assertEquals(40, _subject.getIncomeTax());
 	}
+	
+	@Test
+	public void setSetValidPropertyTax() {
+		_subject = new TaxRates();
+		_subject.setPropertyTax(20);
+		
+		assertEquals(20, _subject.getPropertyTax());
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void setInvalidPropertyTax() {
+		_subject = new TaxRates();
+		_subject.setPropertyTax(-20);
+		fail("Failed to throw exception in failure case");
+	}
+	
+	@Test
+	public void setSetValidBusinessTax() {
+		_subject = new TaxRates();
+		_subject.setBusinessTax(20);
+		
+		assertEquals(20, _subject.getBusinessTax());
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void setInvalidBusinessTax() {
+		_subject = new TaxRates();
+		_subject.setBusinessTax(-20);
+		fail("Failed to throw exception in failure case");
+	}
+	
+	@Test
+	public void setSetValidSalesTax() {
+		_subject = new TaxRates();
+		_subject.setSalesTax(20);
+		
+		assertEquals(20, _subject.getSalesTax());
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void setInvalidSalesTax() {
+		_subject = new TaxRates();
+		_subject.setSalesTax(-20);
+		fail("Failed to throw exception in failure case");
+	}
+	
+	@Test
+	public void setSetValidIncomeTax() {
+		_subject = new TaxRates();
+		_subject.setIncomeTax(20);
+		
+		assertEquals(20, _subject.getIncomeTax());
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void setInvalidIncomeTax() {
+		_subject = new TaxRates();
+		_subject.setIncomeTax(-20);
+		fail("Failed to throw exception in failure case");
+	}
 }
