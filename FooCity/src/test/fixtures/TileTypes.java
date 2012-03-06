d1f2fbb8d6d3b62e2f8d1d0560234f6d7dcaff9f
Adrien Thebo <adrien@puppetlabs.com>
2012-03-06 13:06:03 -0800

    Add forgotten fixture.
    >_>

---
+++ FooCity/src/test/fixtures/TileTypes.java
@@ -0,0 +1,21 @@
+package test.fixtures;
+
+public class TileTypes {
+	public static String[] defaultTypes = {
+		"Beach",
+		"CoalPower",
+		"Commercial",
+		"Dirt",
+		"Forest",
+		"Grass",
+		"Industrial",
+		"NaturalGas",
+		"Park",
+		"PoliceStation",
+		"Residential",
+		"SolarPower",
+		"WaterPlant",
+		"Water",
+		"WindPower",
+	};
+}
