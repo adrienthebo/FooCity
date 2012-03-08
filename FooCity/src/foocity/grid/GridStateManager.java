76797557ce05281bc74f10da47bea64d420bca54
Adrien Thebo <adrien@puppetlabs.com>
2012-03-08 12:43:45 -0800

    Split up logic for grid loading
    Break up the logic in GridStateManager for grid loading into loading
    data from the given file, and then validating and loading it into the
    grid.

--- FooCity/src/foocity/grid/GridStateManager.java
+++ FooCity/src/foocity/grid/GridStateManager.java
@@ -3,6 +3,11 @@ package foocity.grid;
 import java.io.*;
 import java.util.*;
 
+/**
+ * <p>
+ * This class provides methods for loading/saving a grid.
+ * </p>
+ */
 public class GridStateManager {
 
 	/**
@@ -10,16 +15,49 @@ public class GridStateManager {
 	 * Performs an in-place update of a game grid.
 	 * </p>
 	 *
-	 * <p>
-	 * XXX This method only catches and logs errors. Should it just outright
-	 * fail and throw exceptions?
-	 * </p>
-	 *
 	 * @param grid The grid to populate from the file
 	 * @param fileName the path to the game save file.
 	 */
 	public static boolean loadGrid(Grid grid, String fileName) {
 
+		char[][] map = loadMapFile(fileName);
+
+		/* Ensure that the current grid and loaded map are the same
+		 * dimensions; if not then refuse to load.
+		 *
+		 * TODO Allow resizing of the grid and trigger
+		 * PropertyChangedEvents upon resizing.
+		 */
+		int newXSize = map.length;
+		int newYSize = map[0].length;
+
+		if(newXSize != grid.getXSize() || newYSize != grid.getYSize()) {
+			System.err.println("ERROR: Tried to load map file with dimensions " + newXSize + ", " + newYSize);
+			System.err.println("Grid resizing is not currently supported; refusing to load this map.");
+			return false;
+		}
+
+
+		// XXX trigger PropertyChangeEvent?
+		grid.gridFromChars(map);
+		return true;
+	}
+
+	/**
+	 * <p>
+	 * Load a map grid into a 2D array of chars
+	 * </p>
+	 *
+	 * <p>
+	 * XXX This method only catches and logs errors. Should it just outright
+	 * fail and throw exceptions?
+	 * </p>
+	 *
+	 * @param fileName The file to load
+	 * @return The file split into a 2D array of chars
+	 */
+	static char[][] loadMapFile(String fileName) {
+		char[][] rows = null;
 		try {
 			FileReader reader = new FileReader(fileName);
 			BufferedReader stream = new BufferedReader(reader);
@@ -31,28 +69,18 @@ public class GridStateManager {
 				currentRows.add(row);
 				stream.skip(1); // Skip over the newline. XXX Is this necessary?
 			}
-			char[][] rows = (char[][])currentRows.toArray();
+			rows = (char[][])currentRows.toArray();
 
-			/* Generate a new grid from the file, swap out listeners, and
-			 * replace the old grid with the new grid.
-			 *
-			 * REVIEW This is somewhat evil.
-			 */
-			Grid newGrid = new Grid(rows);
-			newGrid._listeners = grid._listeners;
-			grid = newGrid; // XXX This needs to be tested; I'm not sure it'll do what I expect
-			return true;
 		}
 		catch(FileNotFoundException e) {
 			System.err.println("Unable to load grid file " + fileName + ": " + e);
 			e.printStackTrace();
-			return false;
 		}
 		catch(IOException e) {
 			System.err.println("Error while loading " + fileName + ": " + e);
 			e.printStackTrace();
-			return false;
 		}
+		return rows;
 	}
 
 	/**
