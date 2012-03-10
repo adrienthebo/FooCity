fd8f937aa4f87fdcad68be8a419fc0ab4ed50cd2
Adrien Thebo <adrien@puppetlabs.com>
2012-03-10 15:11:09 -0800

    Refactor GridStateManager into an object
    This should ease the difficulties of testing I/O

--- FooCity/src/foocity/grid/GridStateManager.java
+++ FooCity/src/foocity/grid/GridStateManager.java
@@ -5,26 +5,84 @@ import java.util.*;
 
 /**
  * <p>
- * This class provides methods for loading/saving a grid.
+ * This class provides a manager class to store and restore the state of a grid.
  * </p>
  */
 public class GridStateManager {
 
+	protected Grid _grid;
+
 	/**
 	 * <p>
-	 * Performs an in-place update of a game grid.
+	 * Generate a new GridStateManager referencing an existing grid.
 	 * </p>
 	 *
-	 * @param grid The grid to populate from the file
-	 * @param fileName the path to the game save file.
+	 * @param newGrid The grid to manage.
 	 */
-	public static boolean loadGrid(Grid grid, String fileName) {
+	public GridStateManager(Grid newGrid) {
+		_grid = newGrid;
+	}
 
-		char[][] map = loadMapFile(fileName);
-		if(map == null) {
-			// The loading method already failed and logged errors, MAYDAY MAYDAY WE'RE GOING DOWN EJECT
-			return false;
+	/**
+	 * <p>
+	 * Load a grid state from a specific file
+	 * </p>
+	 *
+	 * @param gridSaveFile The path to the file holding the state
+	 */
+	public boolean load(String gridSaveFile) {
+		try {
+			FileReader reader = new FileReader(gridSaveFile);
+
+			char[][] rows = loadFromStream((Reader)reader);
+			populateGrid(rows);
+
+			return true;
 		}
+		catch(IllegalArgumentException e) {
+			System.err.println("Unable to load grid file " + gridSaveFile + ": " + e);
+			System.err.println("Grid resizing is not currently supported; refusing to load this map.");
+			e.printStackTrace();
+		}
+		catch(FileNotFoundException e) {
+			System.err.println("Unable to load grid file " + gridSaveFile + ": " + e);
+			e.printStackTrace();
+		}
+		catch(IOException e) {
+			System.err.println("Error while loading " + gridSaveFile + ": " + e);
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	/**
+	 * <p>
+	 * Read the contents of a file into a 2D array.
+	 * </p>
+	 */
+	public char[][] loadFromStream(Reader reader) throws IOException {
+
+		BufferedReader stream = new BufferedReader(reader);
+		List<char[]> currentRows = new LinkedList<char[]>();
+
+		while(stream.ready()) {
+			char[] row = stream.readLine().toCharArray();
+			currentRows.add(row);
+			stream.skip(1); // Skip over the newline. XXX Is this necessary?
+		}
+		char[][] rows = (char[][])currentRows.toArray();
+
+		return rows;
+	}
+
+	/**
+	 * <p>
+	 * Performs an in-place update of the game grid.
+	 * </p>
+	 *
+	 * @param map The 2D tile
+	 */
+	public void populateGrid(char[][] map) {
 
 		/* Ensure that the current grid and loaded map are the same
 		 * dimensions; if not then refuse to load.
@@ -35,56 +93,13 @@ public class GridStateManager {
 		int newXSize = map.length;
 		int newYSize = map[0].length;
 
-		if(newXSize != grid.getXSize() || newYSize != grid.getYSize()) {
-			System.err.println("ERROR: Tried to load map file with dimensions " + newXSize + ", " + newYSize);
-			System.err.println("Grid resizing is not currently supported; refusing to load this map.");
-			return false;
+		if(newXSize != _grid.getXSize() || newYSize != _grid.getYSize()) {
+			throw new IllegalArgumentException("Tried to load grid from array with mismatched size");
 		}
 
 
 		// XXX trigger PropertyChangeEvent?
-		grid.gridFromChars(map);
-		return true;
-	}
-
-	/**
-	 * <p>
-	 * Load a map grid into a 2D array of chars
-	 * </p>
-	 *
-	 * <p>
-	 * XXX This method only catches and logs errors. Should it just outright
-	 * fail and throw exceptions?
-	 * </p>
-	 *
-	 * @param fileName The file to load
-	 * @return The file split into a 2D array of chars
-	 */
-	static char[][] loadMapFile(String fileName) {
-		char[][] rows = null;
-		try {
-			FileReader reader = new FileReader(fileName);
-			BufferedReader stream = new BufferedReader(reader);
-
-			List<char[]> currentRows = new LinkedList<char[]>();
-
-			while(stream.ready()) {
-				char[] row = stream.readLine().toCharArray();
-				currentRows.add(row);
-				stream.skip(1); // Skip over the newline. XXX Is this necessary?
-			}
-			rows = (char[][])currentRows.toArray();
-
-		}
-		catch(FileNotFoundException e) {
-			System.err.println("Unable to load grid file " + fileName + ": " + e);
-			e.printStackTrace();
-		}
-		catch(IOException e) {
-			System.err.println("Error while loading " + fileName + ": " + e);
-			e.printStackTrace();
-		}
-		return rows;
+		_grid.gridFromChars(map);
 	}
 
 	/**
@@ -97,27 +112,30 @@ public class GridStateManager {
 	 * fail and throw exceptions?
 	 * </p>
 	 *
-	 * @param grid The grid to use to populate the file.
 	 * @param fileName the path to the game save file.
 	 */
-	public static boolean saveGrid(Grid grid, String fileName) {
+	public boolean save(String fileName) {
 
 		try {
 			FileWriter writer = new FileWriter(fileName);
-			BufferedWriter stream = new BufferedWriter(writer);
-
-			char[][] rows = grid.toCharGrid();
-
-			for(char[] row : rows) {
-				stream.write(row, 0, row.length);
-				stream.newLine();
-			}
+			writeToStream(writer);
 			return true;
 		}
 		catch(IOException e) {
 			System.err.println("Error while saving " + fileName + ": " + e);
 			e.printStackTrace();
 			return false;
+		}
+	}
+
+	public void writeToStream(Writer writer) throws IOException {
+		BufferedWriter stream = new BufferedWriter(writer);
+
+		char[][] rows = _grid.toCharGrid();
+
+		for(char[] row : rows) {
+			stream.write(row, 0, row.length);
+			stream.newLine();
 		}
 	}
 }
