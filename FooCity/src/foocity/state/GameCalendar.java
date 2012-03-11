7b2705458028d21898e0a18b43dae4149f1402b2
Adrien Thebo <adrien@puppetlabs.com>
2012-03-11 10:22:24 -0700

    Improve GameCalendar javadoc.

--- FooCity/src/foocity/state/GameCalendar.java
+++ FooCity/src/foocity/state/GameCalendar.java
@@ -8,24 +8,40 @@ import javax.swing.event.EventListenerList;
 
 import foocity.event.*;
 /**
+ * <p>
  * This class forms a thin wrapper around the java.util.Calendar, with a
  * simpler interface and support for Event Listeners.
+ * </p>
  */
 public class GameCalendar implements PropertyChangeGenerator {
 
 	private EventListenerList _listeners = new EventListenerList();
 	private Calendar _calendar;
 
+	/**
+	 * <p>
+	 * Instantiates a new calendar at the current date.
+	 * </p>
+	 */
 	public GameCalendar() {
 		_calendar = new GregorianCalendar();
 	}
 
+	/**
+	 * <p>
+	 * Instantiates a new calendar at a specified date.
+	 * </p>
+	 *
+	 * @param newCalendar The calendar representing the new date.
+	 */
 	public GameCalendar(Calendar newCalendar) {
 		_calendar = newCalendar;
 	}
 
 	/**
+	 * </p>
 	 * Adds a month to the current date.
+	 * </p>
 	 */
 	public void increment() {
 		Object oldCalendar = _calendar.clone();
